4de1113139eb9568a7365f8ac2a28f312f7fb4a4
Ankit Kumar <ankit@ipapagari.com>
2017-05-23 14:24:48 +0530

    implemented layouts

--- imports/startup/client/router.js
+++ imports/startup/client/router.js
@@ -1,6 +1,13 @@
 import { Router } from 'meteor/iron:router'
+
+import '../../ui/templates/layouts/mainLayout'
 import '../../ui/templates/home/home'
 
-Router.route('/', function () {
+
+Router.route('/', {
+  name: 'index',
+  layoutTemplate: 'mainLayout',
+  action() {
     this.render('home');
+  }
 });
