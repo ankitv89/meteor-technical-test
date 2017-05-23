f5b1fbfa924fa040e72e75dc9af6e09103c7faab
Ankit Kumar <ankit@ipapagari.com>
2017-05-23 14:43:55 +0530

    added update method to api

--- imports/api/methods/methods.js
+++ imports/api/methods/methods.js
@@ -24,3 +24,25 @@ export const taskAdd = new ValidatedMethod({
     return;
   },
 });
+
+export const taskUpdate = new ValidatedMethod({
+  name: 'task.update',
+  validate(params) {
+    check(params, {
+      _id: String,
+      modifier: Object,
+    });
+  },
+  run(params) {
+    const { _id, modifier } = params;
+
+    try {
+      // not sure if this is recommneded way to get the modifier from quickform
+      let res = Tasks.update({ _id }, modifier);
+      return { sucess: res };
+    } catch (e) {
+        throw new Meteor.Error('error-500', 'something went wrong on server', { method: 'task.add' });
+    }
+    return;
+  },
+});
