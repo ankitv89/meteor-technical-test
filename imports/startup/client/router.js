6663ae2b961914d0e4b3ff4d83d39485292d5b85
Ankit Kumar <ankit@ipapagari.com>
2017-05-23 14:40:36 +0530

    added update task template and route

--- imports/startup/client/router.js
+++ imports/startup/client/router.js
@@ -1,14 +1,26 @@
 import { Router } from 'meteor/iron:router'
+import { Tasks } from '../../api/collections/collections'
 
 import '../../ui/templates/layouts/mainLayout'
 import '../../ui/templates/home/home'
-
+import '../../ui/templates/task/updateTask'
 
 Router.route('/', {
   name: 'index',
   layoutTemplate: 'mainLayout',
   action() {
     this.render('home');
+  }
+});
+
+Router.route('/task/:id', {
+  name: 'updateTask',
+  layoutTemplate: 'mainLayout',
+  action() {
+    this.render('updateTask');
+  },
+  data: function() {
+    return Tasks.findOne({_id: this.params.id},{fields: { name: true }});
   }
 });
 
